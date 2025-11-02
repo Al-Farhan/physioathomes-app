343b9659abc64a37a58cbf4946c021c676d6edef
Farhan Shaikh <programmingwithfarhan@gmail.com>
2025-11-02 19:56:11 +0530

    setup project

---
+++ src/app/_layout.tsx
@@ -0,0 +1,27 @@
+import { DefaultTheme, ThemeProvider } from "@react-navigation/native";
+import { Stack } from "expo-router";
+import { StatusBar } from "expo-status-bar";
+import "../../global.css";
+
+import { useColorScheme } from "@/src/hooks/use-color-scheme";
+
+export const unstable_settings = {
+  anchor: "(tabs)",
+};
+
+export default function RootLayout() {
+  const colorScheme = useColorScheme();
+
+  return (
+    <ThemeProvider value={DefaultTheme}>
+      <Stack>
+        <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
+        <Stack.Screen
+          name="modal"
+          options={{ presentation: "modal", title: "Modal" }}
+        />
+      </Stack>
+      <StatusBar style="dark" />
+    </ThemeProvider>
+  );
+}
